3210d9aada192c0c1ee5c7abd2e471bf1b920334
MinestoPix <zan.stopar@gmail.com>
2013-10-24 19:24:22 +0200

    something

--- src/tut78/apples.java
+++ src/tut78/apples.java
@@ -6,10 +6,7 @@ public class apples {
 	
 	public static void main(String[] args) {
 		
-		Gui go = new Gui();
-		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		go.setSize(300,200);
-		go.setVisible(true);
+		
 		
 	}
 
